d865e1a9bd2d45ef79b7f27578d25fffd3121c46
jacomer2 <69131100+jacomer2@users.noreply.github.com>
2024-07-16 16:56:17 -0400

    Added basic function outlines

--- Program.cs
+++ Program.cs
@@ -1,10 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
+using System.Numerics;
+
 class MathGame
 {
     static void Main(String[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        bool quit = false;
+
+        while(!quit)
+        {
+            Console.WriteLine("What game would you like to play today? Choose from the options below:");
+            Console.WriteLine("V - View Previous Games");
+            Console.WriteLine("A - Addition");
+            Console.WriteLine("S - Subtraction");
+            Console.WriteLine("M - Multiplication");
+            Console.WriteLine("D - Division");
+            Console.WriteLine("Q - Quit the program");
+            Console.WriteLine("----------------------------------------------");
+
+            String res = Console.ReadLine();
+
+            switch (res.ToUpper())
+            {
+                case "V":
+                    PreviousGames();
+                    break;
+                case "A":
+                    Addition();
+                    break;
+                case "S":
+                    Subtraction();
+                    break;
+                case "M":
+                    Multiplication();
+                    break;
+                case "D":
+                    Division();
+                    break;
+                case "Q":
+                    return;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    break;
+
+            }
+
+            if (res != null && res.ToUpper().Equals("Q"))
+            {
+                return;
+            }
+        }
+
     }
+
+    static void PreviousGames()
+    {
+        Console.WriteLine("In previous games");
+        return;
+    }
+
+    static void Addition()
+    {
+        Console.WriteLine("In addition");
+        return;
+    }
+
+    static void Subtraction()
+    {
+        Console.WriteLine("In subtraction");
+        return;
+    }
+
+    static void Multiplication()
+    {
+        Console.WriteLine("In multiplication");
+        return;
+    }
+
+    static void Division()
+    {
+        Console.WriteLine("In division");
+        return;
+    }
+
 }
